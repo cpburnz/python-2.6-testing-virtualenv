119865df70bbe40c70f1547e8b87611b5721704b
cpburnz <cpburnz@gmail.com>
2018-12-01 11:46:35 -0500

    Remove unneeded reference to original package

--- test_py26.sh
+++ test_py26.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This script tests Python 2.6 using the virtual environment at ".py26-env".
-# This is required because Tox has removed support for Python 2.6.
+# This is simplifies testing because Tox no longer supports Python 2.6.
 #
 set -e
 
@@ -9,11 +9,11 @@ set -e
 cd .py26-env
 source bin/activate
 
-# Install pathspec.
+# Install local package.
 cd ..
 python setup.py clean --all
 python setup.py build
 python setup.py install
 
-# Test pathspec.
+# Test local package.
 python setup.py test
